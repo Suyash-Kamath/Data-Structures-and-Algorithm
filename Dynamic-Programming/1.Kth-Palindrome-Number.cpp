ed9c75bdbf4e03da25216add003ef3ac7a205469
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-08-20 18:22:04 +0530

    Create 1.Kth-Palindrome-Number.cpp

---
+++ Dynamic-Programming/1.Kth-Palindrome-Number.cpp
@@ -0,0 +1,71 @@
+// 0 1 1 2 3 5 8 .....
+
+// Recursion Approach
+/*
+
+#include <iostream>
+using namespace std;
+
+int fibonacci(int k) {
+    if (k <= 1) return k;  // Base cases: fibonacci(0) = 0, fibonacci(1) = 1
+    return fibonacci(k - 1) + fibonacci(k - 2);  // Recursive cases
+}
+
+int main() {
+    int k;
+    cout << "Enter the number: ";
+    cin >> k;
+
+    cout << "Fibonacci(" << k << ") = " << fibonacci(k) << endl;
+
+    return 0;
+}
+
+*/
+
+// Dynamic Programming
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+
+*/
+
+// Optimized way
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  
+  int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
